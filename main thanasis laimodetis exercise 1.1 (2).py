e3a58bc19fd16cb00bf688e00bd99491898d6f31
thanasislaimodetis-tech <thanasislaimodetis@gmail.com>
2025-10-13 14:14:26 +0300

    Add files via upload

---
+++ main thanasis laimodetis exercise 1.1 (2).py
@@ -0,0 +1,12 @@
+import datetime
+
+current_time = datetime.datetime.now()
+
+print("Hello and Welcome to Programming in Python.")
+
+print("Python is a great general-purpose language and is used by the likes of Google and Facebook.")
+
+print(f"The current date and time is {current_time}")
+
+
+      
---
+++ main thanasis laimodetis exercise 1.2.py
@@ -0,0 +1,5 @@
+x = int(input("Give me a number:\n"))
+y = int(input("Give me a second number:\n"))
+
+multiplying = (x*y)   
+print(f"The result of multiplying {x} and {y} is {multiplying}")
